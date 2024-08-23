e7324ab71ba7acd81d26aa2117bb1df7a6602603
Rishavalpha9 <rishavkumarraj100@gmail.com>
2024-08-24 01:41:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return 
+    
+     let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+let sections=document.querySelectorAll('.section');
+let navLinks=document.querySelectorAll('.header nav a');
+
+window.onscroll=()=>{
+
+
+    sections.forEach(sec=>{
+        let top= window.scrollY;
+        let offset =sec.offsetTop -150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+        if(top>= offset && top < offset + height)
+        {
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+    
+  
+}
+
+export default App
